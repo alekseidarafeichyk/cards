bc00e31a0f96705ed58a560e53b35bee3f5d08fd
alekseidarafeichyk <alekseidarafeichyk@gmail.com>
2020-10-13 17:27:11 +0300

    Updates

---
+++ precache-manifest.c621497eb78e67e76c6c064a4ccfd160.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "51a6e51856fe7f103c885b41b302ed86",
+    "url": "/alekseidarafeichyk/cards/index.html"
+  },
+  {
+    "revision": "ba084ed13fd66dd6862b",
+    "url": "/alekseidarafeichyk/cards/static/css/main.90f2c478.chunk.css"
+  },
+  {
+    "revision": "f0650e32e1a3dcdfd8bc",
+    "url": "/alekseidarafeichyk/cards/static/js/2.217afec1.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/alekseidarafeichyk/cards/static/js/2.217afec1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ba084ed13fd66dd6862b",
+    "url": "/alekseidarafeichyk/cards/static/js/main.8c1607c2.chunk.js"
+  },
+  {
+    "revision": "7fd0e1744b2ad305c48f",
+    "url": "/alekseidarafeichyk/cards/static/js/runtime-main.078c479c.js"
+  }
+]);
