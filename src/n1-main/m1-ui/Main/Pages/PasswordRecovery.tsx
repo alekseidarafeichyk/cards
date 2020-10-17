01516e2aa06d0e7655defae387fed2fb4d939113
alexeisamuta <alexeisamuta@gmail.com>
2020-10-17 14:44:31 +0300

    function change state (PassRec)

--- src/n1-main/m1-ui/Main/Pages/PasswordRecovery.tsx
+++ src/n1-main/m1-ui/Main/Pages/PasswordRecovery.tsx
@@ -2,16 +2,26 @@ import React from 'react';
 import {Input} from "../../common/Input/Input";
 import {useFormik} from "formik";
 import {Button} from "../../common/Button/Button";
+import {setEmailToChangePasswordAC} from "../../../m2-bll/reducers/passwordRecovery";
+import {useDispatch, useSelector} from "react-redux";
+import {RootState} from "../../../m2-bll/store";
 
 export const PasswordRecovery = () => {
+
+    const EmailWithState = useSelector<RootState, string >(state => state.passwordRecover.email)
+    const dispatch = useDispatch()
+
+    const stateChangesF = (email: string) => {
+        dispatch(setEmailToChangePasswordAC(email))
+    }
 
     const formik = useFormik({
         initialValues: {
             email: ''
         },
-        // validate,
         onSubmit: values => {
-            alert(JSON.stringify(values, null, 2));
+            stateChangesF(values.email)
+            // alert(JSON.stringify(values, null, 2));
         },
     });
 
@@ -30,6 +40,7 @@ export const PasswordRecovery = () => {
                     <Button type="submit" name={"Send"}/>
                 </div>
             </form>
+            {EmailWithState}
         </div>
     )
 }
