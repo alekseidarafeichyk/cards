fe6fe9d2281ee515f7ffb2c190184a7063d23324
alexeisamuta <alexeisamuta@gmail.com>
2020-10-17 12:55:54 +0300

    render jsx pages PassRec with formik

--- src/n1-main/m1-ui/Main/Pages/PasswordRecovery.tsx
+++ src/n1-main/m1-ui/Main/Pages/PasswordRecovery.tsx
@@ -1,5 +1,34 @@
 import React from 'react';
+import {Input} from "../../common/Input/Input";
+import {useFormik} from "formik";
 
 export const PasswordRecovery = () => {
-    return <div>PasswordRecovery</div>
+
+    const formik = useFormik({
+        initialValues: {
+            email: ''
+        },
+        // validate,
+        onSubmit: values => {
+            alert(JSON.stringify(values, null, 2));
+        },
+    });
+
+    return (
+        <div>
+            <form onSubmit={formik.handleSubmit}>
+                <div>Email: </div>
+                <Input placeholder={"email"}
+                       id="email"
+                       name="email"
+                       type="email"
+                       onChange={formik.handleChange}
+                       value={formik.values.email}
+                />
+                <div>
+                    <button type="submit">Send</button>
+                </div>
+            </form>
+        </div>
+    )
 }
