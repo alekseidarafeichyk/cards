2ba93b2b4653f80ef91e25f95f7122797c0a511a
alekseidarafeichyk <alekseidarafeichyk@gmail.com>
2020-10-17 17:42:05 +0300

    added Component Loader

--- src/n1-main/m1-ui/Main/Pages/Register.tsx
+++ src/n1-main/m1-ui/Main/Pages/Register.tsx
@@ -3,8 +3,20 @@ import {Input} from '../../common/Input/Input';
 import {Button} from '../../common/Button/Button';
 import style from './Register.module.css'
 import {useFormik} from 'formik';
+import {useDispatch, useSelector} from 'react-redux';
+import {RegisterUserTC, RequestStatusType} from '../../../m2-bll/reducers/registerReducer';
+import {RootState} from '../../../m2-bll/store';
+import {Redirect} from 'react-router-dom';
+import {login} from '../../routes/RoutePass';
+import { Loader } from '../../common/Loader/Loader';
 
 export const Register = () => {
+
+    const dispatch = useDispatch();
+    const error = useSelector<RootState, string>(state => state.register.error)
+    const isRegistered = useSelector<RootState, boolean>(state => state.register.isRegistered)
+    const status = useSelector<RootState,RequestStatusType>(state => state.register.status)
+
 
     const formik = useFormik({
         initialValues: {
@@ -13,9 +25,15 @@ export const Register = () => {
             repeatPassword: '',
         },
         onSubmit: values => {
-            alert(JSON.stringify(values, null, 2));
+            dispatch(RegisterUserTC(values.email, values.password))
         },
     });
+
+    if (isRegistered) {
+        return <Redirect to={login}/>
+    }
+
+    const errorElement =  error ? <div className={style.error}>{error}</div> : null
 
     return (
         <div className={style.containerForm}>
@@ -35,8 +53,13 @@ export const Register = () => {
                        placeholder={'Repeat password'}
                        {...formik.getFieldProps('repeatPassword')}
                 />
-                <Button type="submit" name={'Register'}/>
+                {status === 'loading' ?
+                    <Loader/>
+                    :
+                    <Button type="submit" name={'Register'}/>
+                }
             </form>
+            {errorElement}
         </div>
     )
 }
--- src/n1-main/m2-bll/reducers/registerReducer.ts
+++ src/n1-main/m2-bll/reducers/registerReducer.ts
@@ -1,28 +1,56 @@
-type InitialStateType = {
-    isRegistered: boolean,
-    error: string
-}
+import {Dispatch} from 'redux';
+import {registrationAPI} from '../../m3-dal/api';
 
 const InitialState : InitialStateType = {
     isRegistered: false,
-    error: ''
+    error: '',
+    status: 'idle'
 }
-
-type ActionsType = ReturnType<typeof isRegisterUserAC>
 
 export const registerReducer = (state = InitialState, action: ActionsType) : InitialStateType => {
     switch (action.type) {
-        case 'IS_REGISTER_USER': {
+        case 'REGISTER_USER': {
             return {
                 ...state,
                 isRegistered : action.isRegistered,
                 error: action.error ? action.error : ''
             }
         }
+        case 'SET_STATUS': {
+            return {...state,status : action.status}
+        }
         default :
             return state
     }
 }
 
-export const isRegisterUserAC = (isRegistered : boolean,error= '') =>
-    ({type: 'IS_REGISTER_USER',isRegistered,error} as const)
+//actions
+export const RegisterUserAC = (isRegistered : boolean, error= '') =>
+    ({type: 'REGISTER_USER',isRegistered,error} as const);
+export const SetStatusAC = (status: RequestStatusType) =>
+    ({type: 'SET_STATUS',status} as const);
+
+//thunks
+export const RegisterUserTC = (email: string,password: string) => (dispatch: Dispatch) => {
+            dispatch(SetStatusAC('loading'));
+    registrationAPI(email, password)
+        .then(res => {
+            dispatch(SetStatusAC('succeeded'));
+            dispatch(RegisterUserAC(true));
+        })
+        .catch(error => {
+            dispatch(SetStatusAC('failed'));
+            dispatch(RegisterUserAC(false,error.response.data.error));
+        })
+}
+
+//types
+type InitialStateType = {
+    isRegistered: boolean,
+    error: string,
+    status: RequestStatusType
+}
+type ActionsType =
+    | ReturnType<typeof RegisterUserAC>
+    | ReturnType<typeof SetStatusAC>
+export type RequestStatusType = 'idle' | 'loading' | 'succeeded' | 'failed'
